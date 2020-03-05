bb469dad526a4d79a36e9754e2f3531eda7f2370
TomBee <13107614+TomBurg@users.noreply.github.com>
2020-03-05 11:02:56 -0500

    Add files via upload

---
+++ main/main.py
@@ -0,0 +1,33 @@
+# Test OTA routines
+
+from machine import RTC
+import machine
+import network
+import time
+#import utime
+#from machine import Pin, I2C
+#from neopixel import NeoPixel
+from ssd1306 import SSD1306
+from ssd1306 import SSD1306_I2C
+from ota_updater import OTAUpdater
+
+
+def download_and_install_update_if_available():
+    o = OTAUpdater('https://github.com/TomBurg/esp32ota')
+    o.download_and_install_update_if_available('Roboburg', 'R0b0mon2*')
+
+
+def start():
+    # your custom code goes here. Something like this: ...
+    # from main.x import YourProject
+    # project = YourProject()
+    # ...
+    print("Testing OTA updater - Newer version")
+    time.sleep(1)
+
+def boot():
+    download_and_install_update_if_available()
+    start()
+
+
+boot()
